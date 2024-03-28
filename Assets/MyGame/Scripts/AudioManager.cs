cb53ee08e7bf56651ed104a692f76fc54f34f825
langtu262 <trungkienit87@gmail.com>
2024-03-28 12:52:12 +0700

    1

--- Assets/MyGame/Scripts/AudioManager.cs
+++ Assets/MyGame/Scripts/AudioManager.cs
@@ -3,16 +3,45 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class AudioManager : MonoBehaviour
+    
 {
-    // Start is called before the first frame update
-    void Start()
+    private static AudioManager _instance;
+    public AudioManager Instance
     {
-        
-    }
+        get { return _instance; }
 
-    // Update is called once per frame
-    void Update()
+        set {
+                 _instance = value;
+            }
+    }
+    public AudioClip _backGround;
+    public AudioClip _soundFx;
+    public AudioSource backgroundMusic;
+    public AudioSource soundFx;
+    // Start is called before the first frame update
+    private void Start()
     {
-        
+        if (_instance == null)
+        {
+            _instance = this;
+        }
+        else Destroy(gameObject);
+    }
+    public void PlayBackgroundMusic(AudioClip clip)
+    {
+        backgroundMusic.clip = clip;
+        backgroundMusic.Play();
+    }
+    public void PlaySoundEffectMusic(AudioClip clip)
+    {
+        soundFx.PlayOneShot(clip);
+    }
+    public void BackGroundMusic()
+    {
+        PlayBackgroundMusic(_backGround);
+    }
+    public void SoundEffectMusic()
+    {
+        PlaySoundEffectMusic(_soundFx);
     }
 }
